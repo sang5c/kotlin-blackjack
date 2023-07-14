bc23afa17906e1cb0a8404027a24bc236757b28f
sang5c <skah321@gmail.com>
2023-07-14 23:11:12 +0900

    refactor(Hit): 예외 문구 명확하게 변경

--- src/main/kotlin/blackjack/Hit.kt
+++ src/main/kotlin/blackjack/Hit.kt
@@ -2,7 +2,7 @@ package blackjack
 
 class Hit(vararg cards: PlayingCard) : State {
     init {
-        require(cards.size > 1)
-        require(cards.sumOf { it.score } < 21)
+        require(cards.size >= 2) { "2장 이상의 카드로 이루어져 있어야 합니다." }
+        require(cards.sumOf(PlayingCard::score) < 21) { "21점 이상일 수 없습니다." }
     }
 }
