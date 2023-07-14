0d04611e7f5ad93e2ca3a9078667069489d82209
sang5c <skah321@gmail.com>
2023-07-14 23:09:27 +0900

    test(Hit): 힛은 2장 이상의 카드로 이루어져 있다

--- src/test/kotlin/blackjack/HitTest.kt
+++ src/test/kotlin/blackjack/HitTest.kt
@@ -8,9 +8,16 @@ import org.junit.jupiter.api.Test
 
 class HitTest {
     @Test
-    fun constructor() {
+    fun `힛은 21점 이상일 수 없다`() {
         shouldThrow<IllegalArgumentException> {
             Hit(CLUBS_10, CLUBS_J, CLUBS_A)
+        }
+    }
+
+    @Test
+    fun `힛은 2장 이상의 카드로 이루어져 있다`() {
+        shouldThrow<IllegalArgumentException> {
+            Hit(CLUBS_10)
         }
     }
 }
