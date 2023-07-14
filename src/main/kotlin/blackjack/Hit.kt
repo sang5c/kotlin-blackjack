85ace457f0f0febd3567423c75f22218b26e5230
sang5c <skah321@gmail.com>
2023-07-14 23:22:27 +0900

    refactor(Hit): 부생성자를 통해 vararg 받도록 변경

--- src/main/kotlin/blackjack/Hit.kt
+++ src/main/kotlin/blackjack/Hit.kt
@@ -1,6 +1,8 @@
 package blackjack
 
-class Hit(vararg cards: PlayingCard) : State {
+class Hit(private val cards: List<PlayingCard>) : State {
+    constructor(vararg cards: PlayingCard) : this(cards.toList())
+
     init {
         require(cards.size >= 2) { "2장 이상의 카드로 이루어져 있어야 합니다." }
         require(cards.sumOf(PlayingCard::score) < 21) { "21점 이상일 수 없습니다." }
