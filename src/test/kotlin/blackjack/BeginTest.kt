44334708269b4d793d58c92c1f1933c3c5321101
sang5c <skah321@gmail.com>
2023-07-14 22:48:47 +0900

    test(Begin): 시작 상태에서 ACE와 KING을 받으면 블랙잭이다

--- src/test/kotlin/blackjack/BeginTest.kt
+++ src/test/kotlin/blackjack/BeginTest.kt
@@ -2,6 +2,8 @@ package blackjack
 
 import blackjack.fixture.CLUBS_10
 import blackjack.fixture.CLUBS_2
+import blackjack.fixture.CLUBS_A
+import blackjack.fixture.CLUBS_K
 import io.kotest.matchers.types.shouldBeTypeOf
 import org.junit.jupiter.api.Test
 
@@ -13,5 +15,14 @@ class BeginTest {
         val actual = state.draw(CLUBS_2, CLUBS_10)
 
         actual.shouldBeTypeOf<Hit>()
+    }
+
+    @Test
+    fun `시작 상태에서 ACE와 KING을 받으면 블랙잭이다`() {
+        val state = Begin()
+
+        val actual = state.draw(CLUBS_A, CLUBS_K)
+
+        actual.shouldBeTypeOf<Blackjack>()
     }
 }
