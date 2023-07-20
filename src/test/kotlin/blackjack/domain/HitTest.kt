e7ef923d202f15d768e198f19b09a3f399acbb0a
sang5c <skah321@gmail.com>
2023-07-20 15:20:40 +0900

    test(Hit): 힛에서 카드를 받고 21점 초과이면 버스트이다

--- src/test/kotlin/blackjack/domain/HitTest.kt
+++ src/test/kotlin/blackjack/domain/HitTest.kt
@@ -21,11 +21,20 @@ class HitTest {
     }
 
     @Test
-    fun `힛에서 카드를 받고 21점 미만이면 힛이다`() {
+    fun `힛에서 카드를 받고 21점 이하이면 힛이다`() {
         val state = Hit(CLUBS_2, CLUBS_10)
 
-        val actual = state.draw(CLUBS_4)
+        val actual = state.draw(CLUBS_9)
 
         actual.shouldBeTypeOf<Hit>()
+    }
+
+    @Test
+    fun `힛에서 카드를 받고 21점 초과면 버스트이다`() {
+        val state = Hit(CLUBS_2, CLUBS_10)
+
+        val actual = state.draw(CLUBS_10)
+
+        actual.shouldBeTypeOf<Bust>()
     }
 }
